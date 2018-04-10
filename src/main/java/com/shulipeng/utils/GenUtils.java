7804c6a1e96ff4d691db691f315da01e11e94412
xianyangpeng <969379020@qq.com>
2018-04-10 23:20:11 +0800

    增加对oracle数据库的支持

--- src/main/java/com/shulipeng/utils/GenUtils.java
+++ src/main/java/com/shulipeng/utils/GenUtils.java
@@ -44,10 +44,8 @@ public class GenUtils {
         Configuration config = getConfig();
         //2.表和列类属性填充
         fillTableClassAttr(table,config.getString("needRemovePre"));
-
-        boolean isNeedDate = false;//是否需要导入日期
-        boolean isNeedBigDecimal = false;
-        boolean isLogicDelete = false;//是否软删除
+        // velocity 的context
+        Map<String, Object> vcContext = new HashMap<>(16);
         Iterator<Column> iter = columns.iterator();
         while (iter.hasNext()){
             Column column = iter.next();
@@ -56,15 +54,15 @@ public class GenUtils {
                 table.setPk(column);
             }
 
-            //判断是否有日期类型的行，
+            //判断是否有日期类型的行
             if("Date".equals(column.getAttrType())){
-                isNeedDate = true;
+                vcContext.put("isNeedDate",true);
             }
             if("BigDecimal".equals(column.getAttrType())){
-                isNeedBigDecimal = true;
+                vcContext.put("isNeedBigDecimal",true);
             }
             if("is_delete".equalsIgnoreCase(column.getColumnName()) && "Integer".equals(column.getAttrType())){
-                isLogicDelete = true;
+                vcContext.put("isLogicDelete",true);
             }
 
         }
@@ -80,30 +78,8 @@ public class GenUtils {
         Velocity.init(prop);
 
         //4.封装模板需要的数据信息
-        String module = config.getString("module",table.getClassNameSmall().toLowerCase());
-        Map<String, Object> map = new HashMap<>(16);
-        map.put("tableName", table.getTableName());
-        map.put("comment", table.getComment());
-        map.put("pk", table.getPk());
-        map.put("className", table.getClassName());
-        map.put("classNameSmall", table.getClassNameSmall());
-        //map.put("pathName", conf.getString("package").substring(config.getString("package").lastIndexOf(".") + 1));
-        map.put("columns", table.getColumns());
-        map.put("package", config.getString("packageName"));
-        map.put("author", config.getString("author"));
-        map.put("company", config.getString("company"));
-        map.put("email", config.getString("email"));
-        map.put("datetime", new DateTime().toString("yyyy/MM/dd HH:mm:ss"));
-        map.put("isNeedDate", isNeedDate);
-        map.put("isNeedBigDecimal", isNeedBigDecimal);
-        map.put("isLogicDelete", isLogicDelete);//是否含有软删标志
-        map.put("batchRemove", config.getBoolean("batchRemove",true));//是否含有软删标志
-        map.put("fuzzyLookup", config.getBoolean("fuzzyLookup",false));//是否需要模糊查询
-        map.put("sidePagination", config.getString("sidePagination","server"));//分页方式
-        map.put("fgPlugins", config.getStringArray("fgPlugins"));//前段需要插件
-        map.put("fgPluginAddr", getPluginAddr());//前段所有插件地址
-        map.put("module", module);//分页方式，
-        VelocityContext context = new VelocityContext(map);
+        initVelocityContext(vcContext,table,config);
+        VelocityContext context = new VelocityContext(vcContext);
 
         //4.获取模板文件
         String fgFileType = config.getString("fgFileType",Constant.FG_FILE_TYPE_JSP);
@@ -116,7 +92,7 @@ public class GenUtils {
 
             try {
                 //添加到zip
-                zip.putNextEntry(new ZipEntry(getFileName(template, table.getClassNameSmall(), table.getClassName(),config.getString("package"),module,fgFileType )));
+                zip.putNextEntry(new ZipEntry(getFileName(template, table.getClassNameSmall(), table.getClassName(),config.getString("packageName"),getModuleName(config,table),fgFileType )));
                 IOUtils.write(sw.toString(), zip, "UTF-8");
                 IOUtils.closeQuietly(sw);
                 zip.closeEntry();
@@ -124,6 +100,46 @@ public class GenUtils {
                 throw new RuntimeException("渲染模板失败，表名：" + table.getTableName(), e);
             }
         }
+    }
+
+    /**
+     * 初始化模板需要的数据
+     * @param context
+     * @param table
+     * @param config
+     */
+    private static void initVelocityContext(Map<String, Object> context, Table table, Configuration config) {
+        context.put("tableName", table.getTableName());
+        context.put("comment", table.getComment());
+        context.put("pk", table.getPk());
+        context.put("className", table.getClassName());
+        context.put("classNameSmall", table.getClassNameSmall());
+        context.put("columns", table.getColumns());
+        context.put("package", config.getString("packageName"));
+        context.put("author", config.getString("author"));
+        context.put("company", config.getString("company"));
+        context.put("email", config.getString("email"));
+        context.put("datetime", new DateTime().toString("yyyy/MM/dd HH:mm:ss"));
+        context.put("batchRemove", config.getBoolean("batchRemove",true));//是否批量删除
+        context.put("fuzzyLookup", config.getBoolean("fuzzyLookup",false));//是否需要模糊查询
+        context.put("sidePagination", config.getString("sidePagination","server"));//分页方式
+        context.put("fgPlugins", config.getStringArray("fgPlugins"));//前端需要插件
+        context.put("fgPluginAddr", getPluginAddr());//前端所有插件地址
+        context.put("module", getModuleName(config,table));
+    }
+
+    /**
+     * 获取模块名
+     * @param config
+     * @param table
+     * @return
+     */
+    private static String getModuleName(Configuration config, Table table) {
+        String module = config.getString("module");
+        if(StringUtils.isBlank(module) && table != null){
+            module = table.getClassNameSmall().toLowerCase();
+        }
+        return module;
     }
 
 
@@ -185,16 +201,20 @@ public class GenUtils {
      */
     private static List<String> getTemplate(String dbType,String fgFileType) {
         List<String> templates = new ArrayList<>();
-        templates.add(String.format("templates/vm/%s/domain.java.vm",dbType));
-        templates.add(String.format("templates/vm/%s/dao.java.vm",dbType));
-        templates.add(String.format("templates/vm/%s/mapper.xml.vm",dbType));
-        templates.add(String.format("templates/vm/%s/service.java.vm",dbType));
-        templates.add(String.format("templates/vm/%s/serviceImpl.java.vm",dbType));
-        templates.add(String.format("templates/vm/%s/controller.java.vm",dbType));
+        templates.add("templates/vm/domain.java.vm");
+        templates.add("templates/vm/dao.java.vm");
+        templates.add("templates/vm/service.java.vm");
+        templates.add("templates/vm/serviceImpl.java.vm");
+        templates.add("templates/vm/controller.java.vm");
+        if(Constant.DB_TYPE_MYSQL.equals(dbType)){
+            templates.add("templates/vm/mapperMysql.xml.vm");
+        }else{
+            templates.add("templates/vm/mapperOracle.xml.vm");
+        }
         if(Constant.FG_FILE_TYPE_JSP.equals(fgFileType)){
-            templates.add(String.format("templates/vm/%s/list.jsp.vm",dbType));
-            templates.add(String.format("templates/vm/%s/edit.jsp.vm",dbType));
-            templates.add(String.format("templates/vm/%s/add.jsp.vm",dbType));
+            templates.add("templates/vm/list.jsp.vm");
+            templates.add("templates/vm/%s/edit.jsp.vm");
+            templates.add("templates/vm/%s/add.jsp.vm");
         }
         return templates;
     }
@@ -239,7 +259,7 @@ public class GenUtils {
         //以下需要根据框架分类
         if(Constant.FG_FILE_TYPE_JSP.equals(fgFileType)){
             //mapper
-            if(template.contains("mapper.xml.vm")){
+            if(template.contains("mapperMysql.xml.vm")){
                 return basePath +"mapper" + File.separator + module + File.separator + className + "Mapper.xml";
             }
             //list page
@@ -256,7 +276,7 @@ public class GenUtils {
             }
         }else{
             //mapper
-            if(template.contains("mapper.xml.vm")){
+            if(template.contains("mapperMysql.xml.vm")){
                 return basePath + File.separator + "main" + File.separator + packageName + File.separator +"mapper" + File.separator + classNameSmall + "Mapper.xml";
             }
         }
